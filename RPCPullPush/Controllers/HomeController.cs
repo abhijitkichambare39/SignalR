fe52d96496a3872f601ca09d695f6eedb32c3ab1
unknown <abhijitkichambare39@gmail.com>
2021-10-27 19:25:19 +0530

    SignalR RPC

---
+++ RPCPullPush/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using RPCPullPush.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPCPullPush.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Repository repository = new Repository();
+            List<Employee> emp = repository.GetAllMessages();
+
+            return View();
+            //return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetMessages()
+        {
+            List<Employee> messages = new List<Employee>();
+            Repository r = new Repository();
+            messages = r.GetAllMessages();
+            return Json(messages, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ RPCPullPush/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RPCPullPush.Models
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public int Salary { get; set; }
+        public string DeptName { get; set; }
+        public string Designation { get; set; }
+
+    }
+}
